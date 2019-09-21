d93a63d63eb2c0831dada967491ec61a9769d137
Eronbello <eron.bello@gmail.com>
2019-09-21 19:36:57 -0300

    Auth

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,8 +3,8 @@ v-card.elevation-12
   v-toolbar(color='primary' dark='' flat='')
   v-card-text
     v-form
-      v-text-field(label='Email' name='Email' prepend-icon='fas fa-envelope' type='email')
-      v-text-field#password(label='Password' name='password' prepend-icon='fas fa-lock' type='password')
+      v-text-field(v-model="email" label='Email' name='Email' prepend-icon='fas fa-envelope' type='email')
+      v-text-field#password(v-model="password" label='Password' name='password' prepend-icon='fas fa-lock' type='password')
   v-card-actions
     v-spacer
     v-btn(color='#2196f3' style="color: white !important" @click="clicked") Criar conta
@@ -16,10 +16,27 @@ import {
   mapMutations
 } from 'vuex'
 export default {
+  data: () => ({
+    email: "",
+    password: ""
+  }),
   methods: {
     ...mapMutations('app', ['setLogin']),
     login () {
       this.setLogin(true)
+    },
+    auth () {
+      const data = {
+        email: this.email,
+        password: this.password
+      }
+      return this.$http.post('http://localhost:3000/auth/login', data)
+        .then(response => {
+          this.setLogin(true)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
     clicked () {
       this.$emit('clicked')
